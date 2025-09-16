227452e8b338888a982241c30479e7737a55faa9
frontvan1 <frontvan1@gmail.com>
2025-09-16 12:30:45 +0300

    Fixes

--- src/widgets/home/place-ton.tsx
+++ src/widgets/home/place-ton.tsx
@@ -13,9 +13,9 @@ export const PlaceTon = ({
   setShowReplenishment,
 }: PlaceTonProps) => {
   return (
-    <div className="fixed right-0 bottom-0 left-0 z-[100] h-[70vw] w-[100vw] rounded-t-[50px] border-t-[1.4px] border-[#656565] bg-[#181818] p-[25px_38px_35px]">
+    <div className="fixed right-0 bottom-0 left-0 z-[100] h-[70vw] w-[100vw] rounded-t-[50px] border-t-[1.4px] border-[#656565] bg-[#181818] p-[20px_38px_35px]">
       <div className="relative">
-        <Icons.BalanceBg className="absolute right-[-11.1vw] bottom-[-14vw] h-[70vw]" />
+        <Icons.BalanceBg className="absolute right-[-11.1vw] bottom-[-9vw] h-[70vw]" />
 
         <div className="relative text-center text-[21px] leading-[21px] font-[400]">
           Баланс
@@ -24,7 +24,7 @@ export const PlaceTon = ({
             onClick={() => setShowAddTon(false)}
           />
         </div>
-        <div className="flex flex-col items-center justify-center">
+        <div className="z-[1 relative flex flex-col items-center justify-center">
           <BalanceCounter
             className="mt-[13px] h-[37px] w-[107px] !border-[#656565]"
             count={11}
@@ -35,7 +35,7 @@ export const PlaceTon = ({
             Для ставки с вашего баланса будет списано 10 TON
           </div>
 
-          <div className="flex justify-center gap-[23px] pt-[26px]">
+          <div className="flex justify-center gap-[23px]">
             <Button
               onClick={() => setShowAddTon(false)}
               className="h-[45px] w-[140px] !rounded-full bg-[#262626] text-[15px] leading-[15px] font-[500] backdrop-blur-[10.57px]"
